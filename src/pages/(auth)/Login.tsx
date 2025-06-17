7d9def9b9d7aec4201e86dcc740fcb2eb4dd4581
BradMoyetones <brad.moyetones@gmail.com>
2025-06-17 12:05:29 -0500

    Initial logo

--- src/pages/(auth)/Login.tsx
+++ src/pages/(auth)/Login.tsx
@@ -9,7 +9,6 @@ export default function Login() {
                     <a href="/" className="flex items-center gap-2 font-medium">
                         <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
                             <GalleryVerticalEnd className="size-4" />
-                            <img src="/img/LOGO_FPJD.png" alt="" />
                         </div>
                         React Starter Kit
                     </a>
